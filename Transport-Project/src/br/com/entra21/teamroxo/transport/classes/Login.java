3bbeb1d35ab45f1df63f9edea72c66a8e709fadd
kaka-jaques <kjfakhouri@gmail.com>
2022-06-06 21:28:14 -0300

    updated

--- Transport-Project/src/br/com/entra21/teamroxo/transport/classes/Login.java
+++ Transport-Project/src/br/com/entra21/teamroxo/transport/classes/Login.java
@@ -1,6 +1,6 @@
 package br.com.entra21.teamroxo.transport.classes;
 
-import br.com.entra21.teamroxo.transport.herancas.*;
+import br.com.entra21.teamroxo.transport.herancas.PessoaDados;
 
 public class Login extends PessoaDados {
 
