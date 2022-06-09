2834bcc2b8a0071fbd6541635048100a96e00c9c
Mateus Felipe <mateusgoettems@gmail.com>
2022-06-09 00:31:19 -0300

    cadastroLogin

--- Transport-Project/src/br/com/entra21/teamroxo/transport/Main.java
+++ Transport-Project/src/br/com/entra21/teamroxo/transport/Main.java
@@ -1,10 +1,12 @@
 package br.com.entra21.teamroxo.transport;
 
+import br.com.entra21.teamroxo.transport.classes.Login;
+
 public class Main {
 
 	public static void main(String[] args) {
 		
-		
+		Login.cadastroLogin();
 
 	}
 
--- Transport-Project/src/br/com/entra21/teamroxo/transport/classes/Login.java
+++ Transport-Project/src/br/com/entra21/teamroxo/transport/classes/Login.java
@@ -1,81 +1,93 @@
 package br.com.entra21.teamroxo.transport.classes;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
+
 import br.com.entra21.teamroxo.transport.herancas.*;
 
 public class Login extends PessoaDados {
-  
+
 	static Scanner input = new Scanner(System.in);
 
-	public static void aprender() {
-		byte option;
+	static HashMap<String, String> login = new HashMap<>();
 
-		do {
-			System.out.println("0 - Voltar");
-			System.out.println("1 - Login");
-			System.out.println("2 - Cadastro");
-
-			option = input.nextByte();
-
-			switch (option) {
-
-			case 0:
-				System.out.println("Voltando ao menu anterior..");
-				//fun√ß√£o do menu anterior
-				break;
-
-			case 1:
-				login();
-				break;
-
-			case 2:
-				cadastroLogin();
-				break;
-
-			default:
-				System.out.println("Digite o n√∫mero do menu");
-				break;
-			}
-
-		} while (option != 0);
-
+	public Login(String nome, HashSet<String> user, HashSet<String> email, HashSet<String> cpf, String senha) {
+		super(nome, user, email, cpf, senha);
 	}
 
-	private static void cadastroLogin() {
-		String nome, user, email, cpf, senha1, senha2;
-		
-		System.out.println("Digite seu nome completo: ");
-		nome = input.next();
-		
-		System.out.println("Digite um login: ");
-		user = input.next();
-		
-		System.out.println("Digite seu e-mail: ");
-		email = input.next();
-		
+	public static void cadastroLogin() {
+
+		String cpfLogin, nomeLogin, emailLogin, senhaLogin, userLogin, senha1, senha2;
+		// String[] dados = new String[] {};
+
+		HashMap<String, String[]> login = new HashMap<>();
+
+		// limpa todos os dados
+		login.clear();
+
+		System.out.println("Insira os dados para se cadastrar.");
+
 		System.out.println("Digite seu CPF: ");
-		cpf = input.next();
-		
-		System.out.println("Digite sua senha: ");
-		senha1 = input.next();
-		
-		System.out.println("Repita sua senha: ");
-		senha2 = input.next();
-		
-		//Teste de validade da senha
-		if(senha1==senha2) {
-			System.out.println("Sucesso, seu cadastro foi realizado.");
-			
-		} 
-		else {
-			
-		}
+		cpfLogin = input.next();
+		login.put(cpfLogin, null);
+
+		System.out.println("Digite seu nome completo: ");
+		nomeLogin = input.next();
+
+		System.out.println("Digite seu e-mail: ");
+		emailLogin = input.next();
+
+		System.out.println("Digite seu usu·rio: ");
+		userLogin = input.next();
+
+		do {
+
+			System.out.println("Digite sua senha: ");
+			senha1 = input.next();
+
+			System.out.println("Digite novamente sua senha: ");
+			senha2 = input.next();
+
+			// sempre est· entrando no IF, mesmo com senhas iguais
+			// if (senha1 != senha2) {
+			// System.out.println("As senhas n„o conferem, digite novamente!");
+			// }
+
+		} while (senha1 == senha2);
+
+		senhaLogin = senha1;
+
+		System.out.println("Usu·rio cadastrado com sucesso.");
+
+		login.put(userLogin, new String[] { nomeLogin, userLogin, emailLogin, cpfLogin, senhaLogin });
+
 	}
 
 	private static void login() {
-		
+
+		String cpfLogin, senhaLogin, userLogin, validLogin;
+		HashMap<String, String[]> login = new HashMap<>();
+
+		System.out.println("Insira os dados para o login.");
+
+		// do {
+
+		System.out.println("Digite seu usu·rio: ");
+		userLogin = input.next();
+
+		System.out.println("Digite sua senha: ");
+		senhaLogin = input.next();
+
+		// validLogin = login.get(userLogin);
+
+		// if(userLogin == login.getOrDefault(userLogin, null)) {
+
+		// }
+
+		// }while();
 	}
 
-	
-	
 }
